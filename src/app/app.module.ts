fb3dc659610eeae24bddeaf47c75303edf8f4eb0
iNKidzy <nadq.nadia@gmail.com>
2020-12-08 16:07:09 +0100

    components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ToolbarComponent } from './Toolbar/toolbar/toolbar.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { LoginComponent } from './login/login.component';
+import { CourseComponent } from './course/course.component';
+import { TopicComponent } from './topic/topic.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarComponent,
+    WelcomeComponent,
+    LoginComponent,
+    CourseComponent,
+    TopicComponent
   ],
   imports: [
     BrowserModule
