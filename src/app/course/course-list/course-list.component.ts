43bf30effe85d9435fadd3f3b57dcb469b5e87d6
Martin Emil Wøbbe <54886017+daiperboevs42@users.noreply.github.com>
2020-12-12 18:53:02 +0100

    course-list finished

--- src/app/course/course-list/course-list.component.ts
+++ src/app/course/course-list/course-list.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import {Course} from '../../Shared/Models/course.model';
 import {CourseService} from '../../Shared/Services/course.service';
+import {TopicService} from '../../Shared/Services/topic.service';
+import {Topic} from '../../Shared/Models/topic.model';
+import {Observable} from 'rxjs';
 
 @Component({
   selector: 'app-course-list',
@@ -10,7 +13,8 @@ import {CourseService} from '../../Shared/Services/course.service';
 export class CourseListComponent implements OnInit {
 
   courses: Course[];
-  constructor(private courseService: CourseService) { }
+  topics: Observable<Topic[]>;
+  constructor(private courseService: CourseService, private topicService: TopicService) { }
 
   ngOnInit(): void {
     this.courseService.getCourses()
