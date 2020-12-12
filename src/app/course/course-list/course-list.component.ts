f9fdda33b7276893abcb47b400cf0d2e2a3bef60
Nadia Miteva <56917419+iNkidzy@users.noreply.github.com>
2020-12-12 19:43:22 +0100

    Revert "Course"

--- src/app/course/course-list/course-list.component.ts
+++ src/app/course/course-list/course-list.component.ts
@@ -1,9 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import {Course} from '../../Shared/Models/course.model';
-import {CourseService} from '../../Shared/Services/course.service';
-import {TopicService} from '../../Shared/Services/topic.service';
-import {Topic} from '../../Shared/Models/topic.model';
-import {Observable} from 'rxjs';
 
 @Component({
   selector: 'app-course-list',
@@ -12,15 +7,9 @@ import {Observable} from 'rxjs';
 })
 export class CourseListComponent implements OnInit {
 
-  courses: Course[];
-  topics: Observable<Topic[]>;
-  constructor(private courseService: CourseService, private topicService: TopicService) { }
+  constructor() { }
 
   ngOnInit(): void {
-    this.courseService.getCourses()
-      .subscribe(
-        courses => {
-          this.courses = courses;
-        });
   }
+
 }
