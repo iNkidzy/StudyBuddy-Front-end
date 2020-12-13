0bf8ca334093104e3939661ea50df24548f5ee0b
iNKidzy <nadq.nadia@gmail.com>
2020-12-13 03:46:29 +0100

    topics

--- src/app/topic/topic-page/topic-page.component.ts
+++ src/app/topic/topic-page/topic-page.component.ts
@@ -1,10 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import {TopicService} from '../../Shared/Services/topic.service';
-import {UserService} from '../../Shared/Services/user.service';
+import {ActivatedRoute} from '@angular/router';
+import {CommentService} from '../../Shared/Services/comment.service';
 import {Observable} from 'rxjs';
-import {User} from '../../Shared/Models/user.model';
 import {Topic} from '../../Shared/Models/topic.model';
-import {Usertype} from '../../Shared/Models/usertype.model';
+import {TopicService} from '../../Shared/Services/topic.service';
+
 
 @Component({
   selector: 'app-topic-page',
@@ -12,11 +12,18 @@ import {Usertype} from '../../Shared/Models/usertype.model';
   styleUrls: ['./topic-page.component.css']
 })
 export class TopicPageComponent implements OnInit {
- topics: Observable<Topic[]>;
-  constructor(private topic: TopicService, private userType: UserService) { }
-
+  topics$: Observable<Topic[]>;
+  comment: Comment;
+  id: 1;
+  err: string;
+  constructor(private route: ActivatedRoute,
+              private commentService: CommentService,
+              private topicService: TopicService) { }
   ngOnInit(): void {
-
+    this.commentService.findById(1)
+     .subscribe(comment => {this.comment = comment;
+     });
+    this.topics$ = this.topicService.getTopics();
   }
 
 }
