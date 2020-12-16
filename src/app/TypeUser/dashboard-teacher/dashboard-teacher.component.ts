6ba5e0d7fc2b841f21e3d9827e6420bfcb608e90
Martin Emil Wøbbe <54886017+daiperboevs42@users.noreply.github.com>
2020-12-16 21:48:37 +0100

    dynamic teacher info

--- src/app/TypeUser/dashboard-teacher/dashboard-teacher.component.ts
+++ src/app/TypeUser/dashboard-teacher/dashboard-teacher.component.ts
@@ -2,6 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import {Observable} from 'rxjs';
 import {ActivatedRoute} from '@angular/router';
 import {CommentService} from '../../Shared/Services/comment.service';
+import {UserService} from '../../Shared/Services/user.service';
+import {CourseService} from '../../Shared/Services/course.service';
+import {User} from '../../Shared/Models/user.model';
+import {Course} from '../../Shared/Models/course.model';
 
 @Component({
   selector: 'app-dashboard-teacher',
@@ -9,17 +13,23 @@ import {CommentService} from '../../Shared/Services/comment.service';
   styleUrls: ['./dashboard-teacher.component.css']
 })
 export class DashboardTeacherComponent implements OnInit {
-  userType: string;
-  email: string;
-  comment$: Observable<Comment[]>;
-  id: 1;
 
-  constructor(private route: ActivatedRoute, private commentService: CommentService) { }
+  user: User;
+  course: Course;
+
+  constructor(private route: ActivatedRoute,
+              private userService: UserService,
+              private courseService: CourseService) { }
 
   ngOnInit(): void {
-    this.comment$ = this.commentService.getComments();
-    this.userType = 'user';
-    this.email = 'email@email.com';
+    const currentUser = JSON.parse(localStorage.getItem('currentUser'));
+    this.userService.findById(currentUser.id)
+      .subscribe(user => {this.user = user;
+      });
+    this.courseService.findById(1)
+      .subscribe(course =>
+      {this.course = course;
+      });
   }
 
 }
